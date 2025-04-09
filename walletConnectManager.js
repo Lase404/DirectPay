afcc4981e2d85bdbc32530e6e72fce1950b93c50
Toluwalase Adunbi <adunbi8@gmail.com>
2025-04-09 07:52:04 +0100

    Update walletConnectManager.js

--- walletConnectManager.js
+++ walletConnectManager.js
@@ -1,6 +1,18 @@
 const { SignClient } = require('@walletconnect/sign-client');
 const { getSdkError } = require('@walletconnect/utils');
 const admin = require('firebase-admin');
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => `[${timestamp}] ${level.toUpperCase()}: ${message}`)
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'bot.log', maxsize: 5242880, maxFiles: 5 })
+  ],
+});
+
 
 class WalletConnectManager {
   constructor(projectId) {
