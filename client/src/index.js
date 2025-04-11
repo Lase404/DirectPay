b32a58f875ebeaad5eeb0025defc0ce3ee38928d
Toluwalase Adunbi <adunbi8@gmail.com>
2025-04-11 10:01:04 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { PrivyProvider } from '@privy-io/react-auth';
+import ReactDOM from 'react-dom';
 import ConnectWalletApp from './ConnectWalletApp';
 
-if (!process.env.REACT_APP_PRIVY_APP_ID) {
-  console.error('REACT_APP_PRIVY_APP_ID is not set in environment');
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <PrivyProvider appId={process.env.REACT_APP_PRIVY_APP_ID}>
+ReactDOM.render(
+  <React.StrictMode>
     <ConnectWalletApp />
-  </PrivyProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
