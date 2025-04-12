6bc28a079ac2ce97456b1f42099a2e8978639c5a
Toluwalase Adunbi <adunbi8@gmail.com>
2025-04-12 09:49:44 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,12 @@
+// client/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './ConnectWalletApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
